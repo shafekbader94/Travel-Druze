4820ce9624a38b84ce7ed57a6a73d6d2176ce0a2
Shafek Bader <66371617+shafekbader94@users.noreply.github.com>
2024-02-04 18:09:23 +0200

    Delete dist directory

--- dist/places/places.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Places } from './schema/places.schema';
-import { PlacesService } from './places.service';
-export declare class PlacesController {
-    private readonly placesService;
-    constructor(placesService: PlacesService);
-    create(createPlaces: Places): Promise<Places>;
-    findAll(): Promise<Places[]>;
-    findOne(id: string): Promise<Places>;
-}
